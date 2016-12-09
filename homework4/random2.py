8bce5d05eb7963d322d4d480bd508d6eb2974d6e
a-khmilevskiy <grandmofftank@gmail.com>
2016-12-09 22:31:56 +0600

    still_does-not-work

--- homework4/random2.py
+++ homework4/random2.py
@@ -1,18 +1,26 @@
-
+#-=coding-UTF-8=-
 """Написать программу которая заполняет случайным текстом строку.    
 Попросить ввести  символы пользователя
 Заменить регулярным выражением Это слово на ее 
 ревертированное значение если это слово в начале или конце
 это предложения"""
 
+#импортируем всякое
 import random
 import re
 
-stroka = 'abcdefghijklmnopqrstuvwxyz'
-rand = random.shuffle(stroka,[rand])
+#генерируем список с рандомными символами
+xlist = random.sample('abcdefghijklmnopqrstuvwxyz1234567890',36)
+
+xstring = ''
+#делаем его стрингом
+xstring.join(xlist, sep=None)
+
+print (xstring)
 xinput = input('Введите символы:\n')
 
-if re.match(xinput, rand):
+if re.match(xinput, xstring):
 	print (xinput[::-1])
+
 
 
